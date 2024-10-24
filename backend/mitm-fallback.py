deef5ec01567dcc4b8987b74d9d2027fffe4b5fa
Usagi no Niku <chenbohan911@163.com>
2024-10-25 03:46:47 +0800

    backend: add fallback config

--- backend/mitm-fallback.py
+++ backend/mitm-fallback.py
@@ -2,7 +2,7 @@ import datetime
 import mitmproxy.http
 
 allowed_hosts = ["42.193.74.107", "129.28.248.89", "wq.sys-all.cn"]
-target_url = "https://dxpass.turou.fun/"
+target_url = "http://fallback.dxpass.turou.fun/"  # we don't use https here.
 
 
 def request(flow: mitmproxy.http.HTTPFlow):
