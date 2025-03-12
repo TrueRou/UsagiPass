43f2650ea3455a17924b9291cd0e8da1a4f4aed4
Usagi no Niku <chenbohan911@163.com>
2025-03-12 20:45:46 +0800

    refactor: sperate DXBaseView

---
+++ web/src/views/DXBaseView.vue
@@ -0,0 +1,76 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+import DXRating from '@/components/DXRating.vue';
+import QRCode from '@/components/QRCode.vue';
+import CharaInfo from '@/components/CharaInfo.vue';
+import PlayerInfo from '@/components/PlayerInfo.vue';
+import CardBack from '@/components/CardBack.vue';
+
+defineProps<{
+    preferences: PreferencePublic;
+    timeLimit?: string;
+    maimaiCode?: string;
+    settingsRoute?: string;
+}>()
+
+const router = useRouter();
+
+const r = (image: ImagePublic) => import.meta.env.VITE_URL + "/images/" + image!.id;
+</script>
+<template>
+    <div class="flex items-center justify-center h-full w-full">
+        <div class="flex relative flex-col items-center justify-center h-full">
+            <CardBack :preferences="preferences" />
+            <div class="flex flex-col absolute top-0 w-full h-full">
+                <div class="header-widget flex relative w-full justify-between">
+                    <img class="object-cover w-1/2" :src="r(preferences.passname)">
+                    <DXRating class="w-1/2" :rating="Number(preferences.dx_rating) || 0" />
+                </div>
+                <div class="header-widget flex relative w-full flex-row-reverse">
+                    <PlayerInfo class="w-1/2" :username="preferences.display_name!"
+                        :friend-code="preferences.friend_code!" />
+                </div>
+            </div>
+            <div class="absolute flex flex-col left-0" style="bottom: 8%;">
+                <CharaInfo :chara="preferences.character_name!" :time="timeLimit || '12:00:00'" />
+            </div>
+            <div class="qr-widget absolute" v-if="maimaiCode">
+                <QRCode :content="maimaiCode" :size="preferences.qr_size || 20" />
+            </div>
+            <div class="flex absolute bottom-0 items-center justify-center w-full h-8">
+                <div class="footer-widget flex justify-between py-1 rounded-2xl bg-gray-800 text-white opacity-85">
+                    <p class="footer-text font-sega">{{ preferences.simplified_code }}</p>
+                    <p class="footer-text font-sega">{{ preferences.maimai_version }}</p>
+                </div>
+                <template v-if="settingsRoute">
+                    <div class="p-1 rounded-full bg-white" @click="router.push({ name: settingsRoute })">
+                        <img src="../assets/misc/settings.svg" style="width: 2vh;"></img>
+                    </div>
+                </template>
+            </div>
+        </div>
+    </div>
+</template>
+<style scoped>
+.qr-widget {
+    bottom: 5%;
+    right: 0;
+}
+
+.header-widget {
+    padding-left: 2%;
+    padding-right: 2%;
+    padding-top: 0.4%;
+}
+
+.footer-widget {
+    width: 85%;
+    padding-left: 3%;
+    padding-right: 3%;
+}
+
+.footer-text {
+    font-size: 1.2vh;
+    line-height: 120%;
+}
+</style>
--- web/src/views/DXCardView.vue
+++ web/src/views/DXCardView.vue
@@ -2,11 +2,7 @@
 import { ref } from 'vue';
 import { useCardStore } from '@/stores/card';
 import { useServerStore } from '@/stores/server';
-import DXRating from '@/components/DXRating.vue';
-import CharaInfo from '@/components/CharaInfo.vue';
-import PlayerInfo from '@/components/PlayerInfo.vue';
-import { useRouter } from 'vue-router';
-import CardBack from '@/components/CardBack.vue';
+import DXBaseView from './DXBaseView.vue';
 
 const props = defineProps<{
     uuid: string;
@@ -14,70 +10,18 @@ const props = defineProps<{
 
 const cardStore = useCardStore();
 const serverStore = useServerStore();
-const router = useRouter();
 const cardProfile = ref<CardProfile>(await cardStore.fetchCard(props.uuid));
+const cardPreference = ref<PreferencePublic>(JSON.parse(JSON.stringify(cardProfile.value!.preferences))); // Deep copy
 
-const r = (image: ImagePublic) => import.meta.env.VITE_URL + "/images/" + image!.id;
-
-const prepareDefaultPreferences = () => {
-    cardProfile.value!.preferences.character_name ||= cardProfile.value!.preferences.character.name;
-    cardProfile.value!.preferences.display_name ||= cardProfile.value!.preferences.display_name;
-    cardProfile.value!.preferences.dx_rating ||= String(cardProfile.value.player_rating) || String(cardProfile.value!.player_rating);
-    cardProfile.value!.preferences.maimai_version ||= serverStore.serverMessage!.maimai_version;
+const applyPreferences = () => {
+    cardPreference.value.character_name ||= cardPreference.value.character.name;
+    cardPreference.value.dx_rating ||= String(cardProfile.value.player_rating);
+    cardPreference.value.maimai_version ||= serverStore.serverMessage!.maimai_version;
 }
 
-prepareDefaultPreferences();
+applyPreferences();
 
 </script>
 <template>
-    <div class="flex items-center justify-center h-full w-full">
-        <div class="flex relative flex-col items-center justify-center h-full">
-            <CardBack :preferences="cardProfile?.preferences" />
-            <div class="flex flex-col absolute top-0 w-full h-full">
-                <div class="header-widget flex relative w-full justify-between">
-                    <img class="object-cover w-1/2" :src="r(cardProfile?.preferences.passname)">
-                    <DXRating class="w-1/2" :rating="Number(cardProfile?.preferences.dx_rating) || 0" />
-                </div>
-                <div class="header-widget flex relative w-full flex-row-reverse">
-                    <PlayerInfo class="w-1/2" :username="cardProfile?.preferences.display_name!"
-                        :friend-code="cardProfile?.preferences.friend_code!" />
-                </div>
-            </div>
-            <div class="absolute flex flex-col left-0" style="bottom: 8%;">
-                <CharaInfo :chara="cardProfile?.preferences.character_name!" :time="'12:00:00'" />
-            </div>
-            <div class="flex absolute bottom-0 items-center justify-center w-full h-8">
-                <div class="footer-widget flex justify-between py-1 rounded-2xl bg-gray-800 text-white opacity-85">
-                    <p class="footer-text font-sega">{{ cardProfile?.preferences.simplified_code }}</p>
-                    <p class="footer-text font-sega">{{ cardProfile?.preferences.maimai_version }}</p>
-                </div>
-                <div class="p-1 rounded-full bg-white" @click="router.push({ name: 'preferences' })">
-                    <img src="../assets/misc/settings.svg" style="width: 2vh;"></img>
-                </div>
-            </div>
-        </div>
-    </div>
+    <DXBaseView :preferences="cardPreference" timeLimit="12:00:00" :maimaiCode="null" settingsRoute="preferences" />
 </template>
-<style scoped>
-.qr-widget {
-    bottom: 5%;
-    right: 0;
-}
-
-.header-widget {
-    padding-left: 2%;
-    padding-right: 2%;
-    padding-top: 0.4%;
-}
-
-.footer-widget {
-    width: 85%;
-    padding-left: 3%;
-    padding-right: 3%;
-}
-
-.footer-text {
-    font-size: 1.2vh;
-    line-height: 120%;
-}
-</style>
