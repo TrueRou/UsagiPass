6d93f6af0d03f9254ef952800dfe2f5181662970
Usagi no Niku <chenbohan911@163.com>
2025-04-12 09:42:30 +0800

    improve: remove double layer of api

--- usagipass/app/api/v1/__init__.py
+++ usagipass/app/api/v1/__init__.py
@@ -3,5 +3,5 @@ from fastapi import APIRouter
 from usagipass.app.api.v1 import accounts
 
 
-router = APIRouter(prefix="/api/v1", tags=["v1"])
+router = APIRouter(prefix="/v1", tags=["v1"])
 router.include_router(accounts.router)
